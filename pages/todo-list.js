a6a05e06f37c85c6f3f6fdb575e998c07f603bab
ThalesDaviSouza <thalesdavisoul@gmail.com>
2023-05-18 15:50:37 -0300

    tasks's structure change

--- pages/todo-list.js
+++ pages/todo-list.js
@@ -5,25 +5,23 @@ const todoList = {
             bora: 'bora',
             tasks: [{
                     type:'Personal',
-                    list: {
-                        todo: [{
-                            task:'Make a book',
-                            finished: false
-                        }]
-                    }
+                    todo: 
+                    [{
+                        task:'Make a book',
+                        finished: false
+                    }]
                 },
                 {
                     type:'Carrer',
-                    list: {
-                        todo: [{
-                            task:'Write a program',
-                            finished: false
-                        },
-                        {
+                    todo: 
+                    [{
+                        task:'Write a program',
+                        finished: false
+                    },
+                    {
                             task:'Write 20 lines',
                             finished: true
-                        }
-                    ]}
+                    }]
                 },
             ],
         }
@@ -44,16 +42,15 @@ const todoList = {
             </div>
             <div v-for="category in tasks">
                 <h1>{{ category.type }}</h1>
-                <div v-for="todo in category.list.todo">
-                    <p v-if="!todo.finished">{{ todo.task }}</p>
+                <div v-for="todo in category.todo">
+                <p v-if="!todo.finished">{{ todo.task }}</p>
                 </div>
-                <h3 v-if="category.list.todo.filter(p => p.finished !== false).length > 0">Finished tasks</h3>
-                <div v-for="todo in category.list.todo">
+                <h3 v-if="category.todo.filter(p => p.finished !== false).length > 0">Finished tasks</h3>
+                <div v-for="todo in category.todo">
                     <p v-if="todo.finished">{{ todo.task }}</p>
                 </div>
             </div>
         </div>
-        <button @click="test">Test</button>
     </div>`,
     
     methods: {
@@ -68,7 +65,7 @@ const todoList = {
             
             console.log(newTask)
 
-            this.$data.tasks.find(category => category.type === newTask.type).list.todo.push({task:newTask.task, finished:false})
+            this.$data.tasks.find(category => category.type === newTask.type).todo.push({task:newTask.task, finished:false})
         }
     }
 }
