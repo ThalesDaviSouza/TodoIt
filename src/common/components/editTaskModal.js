6022330c76591e8d04647f14ad5cb14f1cc4c35f
ThalesDaviSouza <thalesdavisoul@gmail.com>
2023-06-10 22:08:34 -0300

    Edit Task Modal Redesign

--- src/common/components/editTaskModal.js
+++ src/common/components/editTaskModal.js
@@ -47,10 +47,6 @@ const editTaskModal = {
         Categories(){
             return this.categories
         },
-
-        endTaskText(){
-            return this.task.finished ? 'Unfinish task' : 'Finish Task'
-        },
     },
 
     components: {
@@ -72,22 +68,35 @@ const editTaskModal = {
         <warningModal :warning="warning" v-show="showWarningModal" @closeModal="closeWarningModal" />
 
         <div id="edit-task-modal" class="modal-body">
-            <div>
+            <div class="close-modal">
+                <svg @click="closeModal" xmlns="http://www.w3.org/2000/svg" width="36" height="36" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </div>
+            <div class="edit-task-header">
                 <input v-model="task.title" @click="saveActualName" type="text" placeholder="Insert Task tittle">
-                <button @click="endTask">{{ endTaskText }}</button>
-                <h3>Due Date:</h3>
-                <input id="task-edit-duedate" type="datetime-local" :value="getDueDate" @input="saveNewDueDate" />
-                <h3>Task Description:</h3>
-                <textarea name="task-edit-description" v-model="task.description"
-                    cols="68" rows="5" placeholder="Insert here the task's description">
+                <svg v-if="!task.finished" @click="endTask" xmlns="http://www.w3.org/2000/svg" width="56" height="56" class="bi bi-check2-circle edit-task-finish" viewBox="0 0 16 16">
+                    <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
+                    <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
+                </svg>
+                <svg v-if="task.finished" @click="endTask" xmlns="http://www.w3.org/2000/svg" width="56" height="56" class="bi bi-x-octagon-fill edit-task-finish" viewBox="0 0 16 16">
+                    <path d="M11.46.146A.5.5 0 0 0 11.107 0H4.893a.5.5 0 0 0-.353.146L.146 4.54A.5.5 0 0 0 0 4.893v6.214a.5.5 0 0 0 .146.353l4.394 4.394a.5.5 0 0 0 .353.146h6.214a.5.5 0 0 0 .353-.146l4.394-4.394a.5.5 0 0 0 .146-.353V4.893a.5.5 0 0 0-.146-.353L11.46.146zm-6.106 4.5L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
+                </svg>
+            </div>
+            <div class="edit-task-body">
+                <h4>Task Description:</h4>
+                <textarea class="edit-task-description" name="task-edit-description" v-model="task.description" placeholder="Insert here the task's description">
                 </textarea>
-            </div>
-            <div>
                 <h4>Category:</h4>
-                <button @click="chooseCategory">{{ btnChooseCategoryText() }}</button>
+                <button class="open-select-modal-btn" @click="chooseCategory">{{ btnChooseCategoryText() }}</button>
+                <h4>Due Date:</h4>
+                <input id="task-edit-duedate" type="datetime-local" :value="getDueDate" @input="saveNewDueDate" />
             </div>
-            <button @click="confirmDelete">Delete Task</button>
-            <button @click="closeModal">Save Task</button>
+            <div class="edit-task-footer">
+                <button class="btn-action" @click="confirmDelete">Delete Task</button>
+                <button class="btn-action" @click="closeModal">Save Task</button>
+            </div>
         </div>
     </div>
     `,
