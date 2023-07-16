6f4bd0b6dcfc72491b08569cd076ac9795fab139
ThalesDaviSouza <thalesdavisoul@gmail.com>
2023-07-16 10:57:15 -0300

    Adding some icons to menu and redone open menu button

--- src/common/components/menu.js
+++ src/common/components/menu.js
@@ -28,12 +28,20 @@ const menu = {
             <div class="menu-body" @click="">
                 <div class="menu-header">
                     <div class="menu-close-btn">
-                        <span @click="closeMenu()">Back</span>
+                        <span @click="closeMenu()">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
+                                <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+                            </svg>
+                        </span>
                     </div>
                     <h1>Todo It</h1>
                     <div class="menu-search">
                         <input v-model="searchInput" type="text" placeholder="Search...">
-                        <button>Search</button>
+                        <button>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" class="bi bi-search" viewBox="0 0 16 16">
+                                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                            </svg>
+                        </button>
                     </div>
                 </div>
                 <div class="menu-body">
--- src/views/todo-list.js
+++ src/views/todo-list.js
@@ -106,7 +106,11 @@ const todoList = {
 
             <h2>Todo List</h2>
             <div class="todo-list">
-                <button class="open-modal-btn" @click="showMenu=true">Open Menu</button>
+                <button class="open-menu-btn" @click="showMenu=true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="36" class="bi bi-list" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+                    </svg>
+                </button>
                 
                 <button class="open-modal-btn" @click="showCreateTaskModal=true">Add Task</button>
                 
